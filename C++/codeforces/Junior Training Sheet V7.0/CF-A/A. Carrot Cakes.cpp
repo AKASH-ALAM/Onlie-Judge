519916fffaac52dc442b677d2556c1c7beffb03a
AKASH-ALAM <mesbahulalamakash2014@gmail.com>
2020-08-01 03:19:39 +0600

    No commit

--- C++/codeforces/Junior Training Sheet V7.0/CF-A/A. Carrot Cakes.cpp
+++ C++/codeforces/Junior Training Sheet V7.0/CF-A/A. Carrot Cakes.cpp
@@ -1,25 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main(){
-    /* ios_base::sync_with_stdio(false);
-    cin.tie(NULL); */
-    int n, t, k, d; cin >> n >> t >> k >> d;
-    int time_left, oven_one, oven_two, total_time, total_cake = 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+ 
+    double n, t, k, d; cin >> n >> t >> k >> d;
+    double time_one, time_tow, cake;
+    time_one = (ceil(n/k)) * t;
 
-    total_time = (n / k) * t;
-    cout << "Total time : " << total_time << endl;
-    oven_two = d + t;
-    time_left = total_time - oven_two;
-    total_cake += k;
-    oven_one = (oven_two / t) * k;
-    total_cake += oven_one;
-    
-    /* cout << "Time left: " << time_left << endl;
-    cout << "Total Cake : " << total_cake << endl; */
-
-    if(total_cake >= n && time_left > 0) cout << "YES" << endl;
+    time_tow = t;
+    cake = k;
+    int i = 2;
+    while (!(cake >= n)){
+        if(t == d) {
+            cake += (k+k);
+            time_tow += t;
+        }
+        else if(t > d){
+            if(time_tow >= d){
+                cake += k;
+                time_tow += d;
+            }
+            if(time_tow >= (t*i)){
+                cake += k;
+                time_tow += (t - d);
+                i++;
+            }
+            //cout <<"Time : " << time_tow << " Cake : " << cake << endl;
+        } else {
+            time_tow += t;
+            cake += k;
+            if(time_tow > d){
+                cake += (k+k);
+            }
+            //cout <<"Time : " << time_tow << " Cake : " << cake << endl;
+        }
+    }
+    //cout << time_one << " " << time_tow << endl;
+    if(time_one > time_tow) cout << "YES" << endl;
     else cout << "NO" << endl;
-
     return 0;
 }
