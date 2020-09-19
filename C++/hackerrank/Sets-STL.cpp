b1b69ef681ceff8225e35b3c06f7d5ec5892778c
AKASH-ALAM <mesbahulalamakash2014@gmail.com>
2020-09-19 17:29:47 +0600

    got ac

--- C++/hackerrank/Sets-STL.cpp
+++ C++/hackerrank/Sets-STL.cpp
@@ -10,38 +10,35 @@
 #define sqr(x)      (x) * (x)
 #define gcd(a, b)   __gcd(a, b)
 #define lcm(a, b)   ((a/gcd(a,b)) * b)
-#define pf(x)         push_front(x)
+#define pf(x) 		push_front(x)
 #define pb(x)       push_back(x)
-#define eb(x)        emplace_back(x)
+#define eb(x)		emplace_back(x)
 #define all(x)      (x).begin(), (x).end()
-#define rall(x)     (x).rbegin(), (x).rend()
-#define sz(x)         (int)x.size()
-#define debug(x)     cerr << #x << " = " << (x) <<endl
-#define Fast_io    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rall(x) 	(x).rbegin(), (x).rend()
+#define sz(x) 		(int)x.size()
+#define debug(x) 	cerr << #x << " = " << (x) <<endl
+#define Fast_io    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 
 const ld PI = acos((ld)-1);
 const int MOD = 1e9+7;
 const ll INF = 1e18; 
 using namespace std;
 
-#define in 1
-#define del 2
-#define fnd 3
-
 int main(){
     Fast_io;
     int q;  cin >> q;
     set <int> s;
     while(q--){
-        int y, x;   cin >> y >> x;
-        if(y == in) s.insert(x);
-        else if(y == del) {
-            s.erase(x);
-        }
-        else if(y == fnd){
-            auto it = find(s.begin(), s.end(), x);
-            if(it != s.end()) cout << "Yes" <<endl;
-            else cout << "No" << endl;
+        int type, value;    cin >> type >> value;
+        switch (type){
+        case 1:
+            s.insert(value);
+            break;
+        case 2:
+            s.erase(value);
+            break;
+        case 3:
+            cout << (s.find(value) == s.end() ? "No" : "Yes") << endl;
         }
     }
     return 0;
