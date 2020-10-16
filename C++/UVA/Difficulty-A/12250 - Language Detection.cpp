15321dc77b8b620f1de575e4342ec2095c1a4200
AKASH-ALAM <mesbahulalamakash2014@gmail.com>
2020-10-16 22:46:57 +0600

    AC

--- C++/UVA/Difficulty-A/12250 - Language Detection.cpp
+++ C++/UVA/Difficulty-A/12250 - Language Detection.cpp
@@ -28,15 +28,15 @@ int main(){
     Fast_io;
     string s;
     int i = 1;
-    while(getline(cin, s)){
+    while(cin >> s){
         if(s[0] == '#') break;
         cout << "Case " << i << ": ";
-        if(s.find("HELLO") != -1) cout << "ENGLISH" << endl;
-        else if(s.find("HOLA") != -1) cout << "SPANISH" << endl;
-        else if(s.find("HALLO") != -1) cout << "GERMAN" << endl;
-        else if(s.find("BONJOUR") != -1) cout << "FRENCH" << endl;
-        else if(s.find("CIAO") != -1) cout << "ITALIAN" << endl;
-        else if(s.find("ZDRAVSTVUJTE") != -1) cout << "RUSSIAN" << endl;
+        if(s == "HELLO") cout << "ENGLISH" << endl;
+        else if(s == "HOLA") cout << "SPANISH" << endl;
+        else if(s == "HALLO") cout << "GERMAN" << endl;
+        else if(s == "BONJOUR") cout << "FRENCH" << endl;
+        else if(s == "CIAO") cout << "ITALIAN" << endl;
+        else if(s == "ZDRAVSTVUJTE") cout << "RUSSIAN" << endl;
         else cout << "UNKNOWN" << endl;
         i++;
     }
